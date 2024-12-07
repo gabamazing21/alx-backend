512b10e0e1a68e00998487a0ca036e3dc16893fd
Gabriel Daramola <gabamazing21@gmail.com>
2024-12-07 06:04:07 +0100

    k

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,21 @@
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('error', err => console.log('Redis client not connected to the server: ', err));
+
+await client.connect();
+
+client.on('connect', () => {
+    console.log('Redis client connected to the server')
+})
+
+function setNewSchool(schoolName, value){
+   client.set(schoolName, value); 
+}
+
+function displaySchoolValue(schoolName){
+    client.get(schoolName); 
+ }
+
+ 
